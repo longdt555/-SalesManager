196e42e8cc2c060ac9b5492b5d5917d08a4896ed
TongDT98 <dinhthethong050698@gmail.com>
2021-03-16 22:42:09 +0700

    update

---
+++ YDManagement/YDManagement/APIControllers/BackendUserController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Lib.Common;
+using Lib.Service.Dtos;
+using Lib.Service.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YDManagement.Authorization;
+using Lib.Data.Entity;
+using Lib.Common.Helpers;
+
+namespace YDManagement.APIControllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackendUserController : BaseController
+    {
+        private readonly IBackendUserService _backendUserService;
+        private readonly IMapper _mapper;
+        public BackendUserController(IBackendUserService backendUserService, IMapper mapper)
+        {
+            _backendUserService = backendUserService;
+            _mapper = mapper;
+        }
+        [Permission(Roles.Administrator)]
+        [HttpPost]
+        public IActionResult Create([FromBody] BackendUserDto model)
+        {
+            var data = _mapper.Map<BackendUser>(model);
+            try
+            {
+                data.CreatedDate = DateTime.Now;
+                _backendUserService.Create(data);
+                var dataDto = _mapper.Map<BackendUserDto>(data);
+                return Ok(dataDto);
+            }
+            catch (AppException ex)
+            {
+
+               return BadRequest(new { message = ex.Message });
+                
+            }
+        }
+        [Permission(Roles.Administrator)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var data = _backendUserService.GetAll();
+            return Ok(data);
+        }
+
+        [Permission(Roles.Administrator)]
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _backendUserService.GetById(id);
+            var dataDto = _mapper.Map<BackendUserDto>(data);
+            return Ok(dataDto);
+        }
+
+        [Permission(Roles.Administrator)]
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] BackendUserDto model)
+        {
+            // map dataDto to entity and set id
+            var data = _mapper.Map<BackendUser>(model);
+            try
+            {
+                data.UpdatedDate = DateTime.Now;                
+                _backendUserService.Update(data);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Permission(Roles.Administrator)]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _backendUserService.Delete(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ YDManagement/YDManagement/APIControllers/RoleController.cs
@@ -0,0 +1,100 @@
+﻿
+using AutoMapper;
+using Lib.Common;
+using Lib.Common.Helpers;
+using Lib.Data.Entity;
+using Lib.Service.Dtos;
+using Lib.Service.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YDManagement.Authorization;
+
+namespace YDManagement.APIControllers
+{
+    [Route("api/[controller]")]
+    [Permission(Roles.Administrator + ", " + Roles.Client)]
+    [ApiController]
+    [Microsoft.AspNetCore.Authorization.Authorize] // The request must be contains jwt
+    public class RoleController : BaseController
+    {
+        private readonly IRoleService _roleService;
+        private readonly IMapper _mapper;
+        public RoleController(IRoleService roleService, IMapper mapper)
+        {
+            _roleService = roleService;
+            _mapper = mapper;
+        }
+        public IActionResult GetAll()
+        {
+            var data = _roleService.GetAll();
+            var dataDto = _mapper.Map<IList<RoleDto>>(data);
+            return Ok(dataDto);
+        }
+
+        // GET api/<RoleController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _roleService.GetById(id);
+            var dataDto = _mapper.Map<RoleDto>(data);
+            return Ok(dataDto);
+        }
+
+        // POST api/<RoleController>
+        [Permission(Roles.Administrator)]
+        [HttpPost]
+        public IActionResult Create([FromBody] RoleDto model)
+        {
+            // map dto to entity
+            var data = _mapper.Map<RoleDto, Role>(model);
+            try
+            {
+                // save 
+                _roleService.Create(data);
+                var dataDto = _mapper.Map<RoleDto>(data);
+                return Ok(dataDto);
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+
+        }
+
+        // PUT api/<RoleController>/5
+        [Permission(Roles.Administrator)]
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] RoleDto model)
+        {
+            // map dto to entity and set id
+            var data = _mapper.Map<Role>(model);
+
+            try
+            {
+                _roleService.Update(data);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        // DELETE api/<RoleController>/5
+        [Permission(Roles.Administrator)]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _roleService.Delete(id);
+            return Ok();
+        }        
+    }
+   
+       
+}
